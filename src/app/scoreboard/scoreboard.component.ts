55e06ddf40229d7f96d573455a920e3b2530497d
ShihShihDer <suc12345@gmail.com>
2024-03-31 03:34:45 +0800

    Refactor scoreboard component to use an array of teams

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,30 +1,63 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-scoreboard',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './scoreboard.component.html',
+//   styleUrl: './scoreboard.component.scss'
+// })
+// export class ScoreboardComponent {
+//   teamAScore: number = 0;
+//   teamBScore: number = 0;
+
+//   incrementScore(team: 'A' | 'B', points: number) {
+//     if (team === 'A') {
+//       this.teamAScore += points;
+//     } else {
+//       this.teamBScore += points;
+//     }
+//   }
+
+//   decrementScore(team: 'A' | 'B', points: number) {
+//     if (team === 'A' && this.teamAScore > 0) {
+//       this.teamAScore -= points;
+//     } else if (team === 'B' && this.teamBScore > 0) {
+//       this.teamBScore -= points;
+//     }
+//   }
+// }
+
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Team {
+  name: string;
+  score: number;
+}
 
 @Component({
   selector: 'app-scoreboard',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './scoreboard.component.html',
-  styleUrl: './scoreboard.component.scss'
+  styleUrls: ['./scoreboard.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class ScoreboardComponent {
-  teamAScore: number = 0;
-  teamBScore: number = 0;
+  teams: Team[] = [
+    { name: 'HOME', score: 0 },
+    { name: 'GUEST', score: 0 }
+  ];
 
-  incrementScore(team: 'A' | 'B', points: number) {
-    if (team === 'A') {
-      this.teamAScore += points;
-    } else {
-      this.teamBScore += points;
+  changeScore(teamIndex: number, delta: number) {
+    const newScore = this.teams[teamIndex].score + delta;
+    if (newScore >= 0) {
+      this.teams[teamIndex].score = newScore;
     }
   }
 
-  decrementScore(team: 'A' | 'B', points: number) {
-    if (team === 'A' && this.teamAScore > 0) {
-      this.teamAScore -= points;
-    } else if (team === 'B' && this.teamBScore > 0) {
-      this.teamBScore -= points;
-    }
+  resetScore() {
+    this.teams.forEach(team => team.score = 0);
   }
 }

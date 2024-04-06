903e06a48489f70c18ee29944ca5a263e8a06948
ShihShihDer <suc12345@gmail.com>
2024-04-06 22:20:12 +0800

    Refactor scoreboard component to improve UI

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,34 +1,3 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-scoreboard',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './scoreboard.component.html',
-//   styleUrl: './scoreboard.component.scss'
-// })
-// export class ScoreboardComponent {
-//   teamAScore: number = 0;
-//   teamBScore: number = 0;
-
-//   incrementScore(team: 'A' | 'B', points: number) {
-//     if (team === 'A') {
-//       this.teamAScore += points;
-//     } else {
-//       this.teamBScore += points;
-//     }
-//   }
-
-//   decrementScore(team: 'A' | 'B', points: number) {
-//     if (team === 'A' && this.teamAScore > 0) {
-//       this.teamAScore -= points;
-//     } else if (team === 'B' && this.teamBScore > 0) {
-//       this.teamBScore -= points;
-//     }
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 

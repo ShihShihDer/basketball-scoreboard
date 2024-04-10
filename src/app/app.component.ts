653c94088c8d1d216b3ee3694ddee993ae798f03
ShihShihDer <suc12345@gmail.com>
2024-04-11 01:13:00 +0800

    Remove BuzzerComponent and its related files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScoreboardComponent } from "./scoreboard/scoreboard.component";
 import { TimerComponent } from "./timer/timer.component";
-import { BuzzerComponent } from "./buzzer/buzzer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, ScoreboardComponent, TimerComponent, BuzzerComponent]
+    imports: [RouterOutlet, ScoreboardComponent, TimerComponent]
 })
 export class AppComponent {
   title = 'basketball-scoreboard';
